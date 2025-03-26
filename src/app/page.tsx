35ce6bdbecb181231d9f91144f4681ce436f22b0
CristianAndriel <cristianandrielsilva@gmail.com>
2025-03-25 23:57:03 -0300

    primeiro commit

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { Montserrat } from "next/font/google";
+import { About } from "./_components/about";
+import { Hero } from "./_components/hero";
+import { Services } from "./_components/services";
+import { Testimonials } from "./_components/testimonials";
+import { Footer } from "./_components/footer";
+import { AosInit } from "./_components/aos-init";
+
+const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "700"] });
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <About />
+      <Services />
+      <Testimonials />
+      <Footer />
+      <AosInit />
+    </main>
+  )
+}
